937752b4f2c32aceefd4d10133509e39f5a6b217
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-06-24 20:44:31 +0600

    updated

--- lib/widgets/icon_button.dart
+++ lib/widgets/icon_button.dart
@@ -11,7 +11,7 @@ class CustomIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: height??50,
+      height: height??80,
       width: width??70,
       decoration: BoxDecoration(
           color: backgroundColor ?? AppColors.secondaryColor,
